342a548ad622e0289b4f01f706c3f71fa43f737e
yedf2 <120050102@qq.com>
2021-12-24 12:27:31 +0800

    not reset job id

--- goveralls.go
+++ goveralls.go
@@ -495,8 +495,6 @@ func process() error {
 		j.SourceFiles = files
 	}
 
-	// TODO I reset job id for github
-	j.ServiceJobID = ""
 	if *debug {
 		j := j
 		if j.RepoToken != nil && *j.RepoToken != "" {
